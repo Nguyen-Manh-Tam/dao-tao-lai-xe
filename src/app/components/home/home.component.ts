e2a5492c52ec061293d9dad794b009ef5f760e79
Nguyen-Manh-Tam <71253904+Nguyen-Manh-Tam@users.noreply.github.com>
2023-02-17 17:12:07 +0700

    113

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	onSubmit(form: NgForm) {
+		if (form.valid) {
+		  console.log(form.value);
+		}
+	  }
 
 }
