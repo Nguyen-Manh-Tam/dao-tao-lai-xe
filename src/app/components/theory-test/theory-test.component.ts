30773e3f1a4da1d1e1e9fcf3f786130020898987
Nguyen-Manh-Tam <71253904+Nguyen-Manh-Tam@users.noreply.github.com>
2023-02-22 14:56:14 +0700

    113

--- src/app/components/theory-test/theory-test.component.ts
+++ src/app/components/theory-test/theory-test.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TheoryTestComponent implements OnInit {
 	filteredStreets: string[] = ['Khái Niệm', 'Quy Tắc Giao Thông', 'Nghiệp Vụ Vận Tải', 'Tốc Độ Khoảng Cách', 'Văn Hóa Giáo Dục', 'Kỹ Thuật Lái Xe', 'Cấu Tạo Sửa Chữa', 'Biển Báo'];
 	isHide: boolean = false;
-	// street: string = 'Khái Niệm';
+	isUse: boolean = false;
+	isStart: boolean = false;
 
 	constructor() { }
 
@@ -17,6 +18,24 @@ export class TheoryTestComponent implements OnInit {
 
 	clickQuestion() {
 		this.isHide = true;
+		this.isUse = false;
+	}
+
+	clickMockTest() {
+		this.isUse = true;
+		this.isHide = false;
+	}
+
+	changeOption(value: any) {
+		if (value) {
+
+		} else {
+			this.isHide = true;
+		}
+	}
+
+	clickStart() {
+
 	}
 
 }
